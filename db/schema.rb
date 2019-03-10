bf3b11204f584bc05a908593b382af9b5884af3f
Axel Tagliapietra <axeltaglia@gmail.com>
2019-03-10 16:19:55 -0300

    Modelo contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180918032111) do
+ActiveRecord::Schema.define(version: 20190310191809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20180918032111) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["student_id"], name: "index_contact_informations_on_student_id", using: :btree
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "course_schedules", force: :cascade do |t|
