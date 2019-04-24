2abd9fc29d073e81396bef6624bf00a7d3e822d5
Axel Tagliapietra <axeltaglia@gmail.com>
2019-04-24 11:27:57 -0300

    inicio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def dashboard_url
 		return new_user_session_url unless current_user
-		return owner_subscriptions_path if current_user.is? :admin
-		return owner_subscriptions_path if current_user.is? :institution_owner
+		return owner_dashboard_path if current_user.is? :admin
+		return owner_dashboard_path if current_user.is? :institution_owner
 		return new_user_session_url
 	end
 
